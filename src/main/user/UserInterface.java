116c3932362277c588a3911521887a6a7109bd8c
jacobjonesgw <jacobjones@gwu.edu>
2015-04-28 07:37:51 -0400

    Word.
    More fixes.

---
+++ src/main/user/UserInterface.java
@@ -0,0 +1,17 @@
+package user;
+public interface UserInterface 
+{
+
+	public String getUsername();
+	
+	public void setUsername(String username);
+	
+	public long getUserID();
+	
+	public void setUserID(long userID);
+	
+	public String getPassword();
+	
+	public void setPassword(String password);
+	
+}
