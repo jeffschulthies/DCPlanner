62a90564d41416cc465fef2d41ac528c1cd5b3f5
jacobjonesgw <jacobjones@gwu.edu>
2015-04-27 23:41:08 -0400

    Edited PlanScreen3, it almost works.
    Almost being the keyword.

--- src/main/PlanScreen3.java
+++ src/main/PlanScreen3.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import itinerary.Itinerary;
 import location.types.Activity;
@@ -8,23 +10,64 @@ public class PlanScreen3
 
     private PanelHandler handler;
     private JPanel mainPanel;
+    private JPanel activityPanel;
     private JButton previousButton;
     private JButton nextButton;
     private JButton addButton;
-    private JPanel activityPanel;
+    private JLabel insertNameLabel;
+    private JLabel insertAddressLabel;
+    private JLabel insertDescriptionLabel;
+    private JLabel insertPriceLabel;
+    private JLabel descriptionLabel;
+    private JLabel priceLabel;
     private Itinerary currentItinerary;
     private ArrayList<Activity> potentialActivityList;
+    private int currentTime;
+    private int currentActivityIndex;
 
     public PlanScreen3(PanelHandler panels, Itinerary parentItinerary)
     {
         this.handler = panels;
         this.currentItinerary = parentItinerary;
+        this.currentTime = this.currentItinerary.getTripStartTime();
+        this.currentActivityIndex = 0;
         this.parseActivities();
+        this.setLabels();
+        this.nextButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                if (currentActivityIndex != potentialActivityList.size() - 1)
+                {
+                    currentActivityIndex++;
+                    setLabels();
+                }
+            }
+        });
+        this.previousButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                if (currentActivityIndex != 0)
+                {
+                    currentActivityIndex--;
+                    setLabels();
+                }
+            }
+        });
     }
 
     public JPanel getMainPanel()
     {
         return this.mainPanel;
+    }
+
+    public void setLabels()
+    {
+        this.insertNameLabel.setText(this.potentialActivityList.get(this.currentActivityIndex).getName());
+        this.insertAddressLabel.setText(this.potentialActivityList.get(this.currentActivityIndex).getAddress());
+        this.insertDescriptionLabel.setText(this.potentialActivityList.get(this.currentActivityIndex).getDescription());
+        this.insertPriceLabel.setText(Double.toString(this.potentialActivityList.get(this.currentActivityIndex).getPrice()));
     }
 
     public void parseActivities()
